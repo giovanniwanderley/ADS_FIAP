9c1e1e0ad6157012d1a84b27166d9675698c25b1
Giovanni Wanderley <assuncaogiovanni1@gmail.com>
2024-04-24 23:46:53 -0300

    Organizando o Repositório

--- ATIVIDADES/cap 2.py
+++
@@ -1,6 +0,0 @@
-contadora = 0 
-numero = int(input('Por favor, insira o número da tabuada'))
-while contadora <= 10:
-    resultado = numero * contadora
-    print (f'{numero} X {contadora} = {resultado}')
-    contadora = contadora + 1  
--- ATIVIDADES/exercicio.py
+++
@@ -1,3 +0,0 @@
-cont = 1 
-soma = 0
-while cont
